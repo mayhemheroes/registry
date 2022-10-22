e369f348743ee5518f22b95dd8a519e49ca8d165
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-21 23:21:44 -0700

    updated fuzz function

--- mayhem/fuzz.go
+++ mayhem/fuzz.go
@@ -1,9 +1,11 @@
 package fuzz
 
 import "strconv"
+import "context"
 import "github.com/apigee/registry/cmd/registry/controller"
 import "github.com/apigee/registry/rpc"
 import "github.com/apigee/registry/cmd/registry/core"
+import "github.com/apigee/registry/cmd/registry/scoring"
 
 func mayhemit(bytes []byte) int {
 
@@ -15,11 +17,27 @@ func mayhemit(bytes []byte) int {
     }
 
     switch num {
+    case 0:
+        core.NewLintFromZippedProtos("mayhem", bytes)
+        return 0
 
     case 1:
         content := string(bytes)
         var test rpc.Manifest
         controller.ValidateManifest(content, &test)
+        return 0
+
+    case 2:
+        content := string(bytes)
+        ctx := context.Background()
+        var test rpc.Artifact
+        core.ExportVersionHistoryToSheet(ctx, content, &test)
+        return 0
+
+    case 3:
+        content := string(bytes)
+        var test rpc.ScoreDefinition
+        scoring.ValidateScoreDefinition(content, &test)
         return 0
 
     default:
